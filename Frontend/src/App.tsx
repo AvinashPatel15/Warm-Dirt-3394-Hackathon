3e2594f9311ddd6f237b0235ce492fd4b818555f
sujjwal21 <sujjwal85@gmail.com>
2023-03-03 01:40:02 +0530

    grid alignment done

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,14 +6,15 @@ import { shuffleArray } from './utils';
 // Types
 import { CardType } from './setup';
 // Styles
-import { Grid } from './App.style';
+import { Grid } from "@chakra-ui/react"
 
 const App = () => {
   const [cards, setCards] = React.useState<CardType[]>(shuffleArray(createBoard()));
   const [gameWon, setGameWon] = React.useState(false);
   const [matchedPairs, setMatchedPairs] = React.useState(0);
   const [clickedCard, setClickedCard] = React.useState<undefined | CardType>(undefined);
-
+const startTime=new Date().toTimeString()
+console.log(startTime)
   React.useEffect(() => {
     if (matchedPairs === cards.length / 2) {
       console.log('Game Won!');
@@ -58,10 +59,33 @@ const App = () => {
 
     setClickedCard(undefined);
   };
-
+/**
+ * templateColumns={{
+              base: `repeat(1,1fr)`,
+              sm: `repeat(2,1fr)`,
+              md: `repeat(3,1fr)`,
+              lg: `repeat(4,1fr)`,
+            }}
+            margin={"auto"}
+            gap={6}
+ */
   return (
-    <div>
-      <Grid>
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center",marginTop:"10px"}}>
+      <Grid 
+        templateRows={{
+          base: `repeat(4,70px)`,
+          sm: `repeat(4,140px)`,
+          md: `repeat(4,160px)`,
+          lg: `repeat(4,180px)`,
+        }}
+        templateColumns={{
+          base: `repeat(4,70px)`,
+          sm: `repeat(4,120px)`,
+          md: `repeat(4,140px)`,
+          lg: `repeat(4,180px)`,
+        }}
+        gap={4}
+      >
         {cards.map(card => (
           <Card key={card.id} card={card} callback={handleCardClick} />
         ))}
