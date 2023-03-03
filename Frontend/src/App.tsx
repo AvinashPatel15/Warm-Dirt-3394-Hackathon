4ff5b41b4b488e7cfda47ca2008b170fac00bd57
Akash Kumar Singh <akashviratsingh018@gmail.com>
2023-03-03 12:18:42 +0530

    added: leaderboard page with responsiveness

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
+import Leaderboard from "./Pages/Leaderboard";
 
 function App() {
   return (
     <>
-      <Box>Hello World!</Box>
+      <Leaderboard></Leaderboard>
     </>
   );
 }
