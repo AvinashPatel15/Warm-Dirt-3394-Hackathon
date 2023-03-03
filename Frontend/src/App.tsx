1ac8e2edc266f7ec0e157f4f9f9ee9c3ed6d6ac7
Rashmigautam16 <rashmigaut99@gmail.com>
2023-03-03 17:57:43 +0530

    login signup

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
+import Login  from "./Pages/Login";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
     <>
       <Box>Hello World!</Box>
+      {/* <Login/> */}
+      <Signup/>
     </>
   );
 }
