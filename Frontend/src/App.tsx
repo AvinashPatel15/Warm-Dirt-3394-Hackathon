373a65930f26bb14784ebf4bd8b8a84562246e9e
sujjwal21 <sujjwal85@gmail.com>
2023-03-04 00:10:20 +0530

    navbar added

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 import Allroutes from "./Routes/Allroutes";
 
 const App = () => {
   return (
     <>
+    <Navbar/>
       <Allroutes />
     </>
   );
